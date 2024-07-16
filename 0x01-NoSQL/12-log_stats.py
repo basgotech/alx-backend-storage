8293f9233e956cd266e758442a6bedb131b38fc1
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-16 10:46:26 -0700

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-excutes all log status
+""" 12. Log stats
 """
 
 
@@ -8,12 +7,11 @@ from pymongo import MongoClient
 
 
 def log_stats():
+    """ log_stats.
     """
-    commit all query
-    """
-    user = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = user.logs.nginx
-    total_count = logs_collection.count_documents({})
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
     get = logs_collection.count_documents({"method": "GET"})
     post = logs_collection.count_documents({"method": "POST"})
     put = logs_collection.count_documents({"method": "PUT"})
@@ -21,7 +19,7 @@ def log_stats():
     delete = logs_collection.count_documents({"method": "DELETE"})
     path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{total_count} logs")
+    print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
     print(f"\tmethod POST: {post}")
