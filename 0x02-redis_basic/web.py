e8c4fbfefddd2d5daf1460481330adb663058ade
Basliel Tegegn <basgolilo5@gmail.com>
2024-07-17 08:59:39 -0700

    fix git push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,32 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" Implementing an expiring web cache and tracker """
+
+from functools import wraps
 import redis
 import requests
-from functools import wraps
 from typing import Callable
 
-
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+rad_getter_ = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_requests(method: Callable) -> Callable:
+    """ wrapper setter """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        rad_getter_.incr(f"count:{url}")
+        ht = rad_getter_.get(f"cached:{url}")
+        if ht:
+            return ht.decode('utf-8')
+        html_code = method(url)
+        rad_getter_.setex(f"cached:{url}", 10, html_code)
+        return html_code
+
+    return wrapper
 
 
-@data_cacher
+@count_requests
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """ RUN HTML CODE """
+    requests = requests.get(url)
+    return requests.text
